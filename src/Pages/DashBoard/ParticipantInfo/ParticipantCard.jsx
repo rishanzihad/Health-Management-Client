85ecafc6ec756de3384f9b25a09c56c2fc9c2d19
rishanzihad <138320123+rishanzihad@users.noreply.github.com>
2023-11-27 23:03:03 +0600

    payment Status change

--- src/Pages/DashBoard/ParticipantInfo/ParticipantCard.jsx
+++ src/Pages/DashBoard/ParticipantInfo/ParticipantCard.jsx
@@ -2,36 +2,50 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import useAxiosSecure from '../../Hooks/useAxiosSecure';
+import toast from 'react-hot-toast';
 
 const ParticipantCard = ({ regi }) => {
   const axiosSecure = useAxiosSecure();
 
-  const { campName, email, phone, gender, healthInfo, emergencyContact } = regi;
-  const { data: payment = [] } = useQuery({
+  const { name,campName, email, phone, gender, healthInfo, emergencyContact } = regi;
+  const { data: payment = [],refetch } = useQuery({
     queryKey: ['payment'],
     queryFn: async () => {
       const res = await axiosSecure.get('/payment');
+
       return res.data;
     },
   });
 
-  // Find the payment for the specific participant
   const participantPayment = payment.find((pay) => pay.email === email);
-
+  
+  //console.log(participantPayment)
+const handleStatus =()=>{
+    axiosSecure.patch(`/payment/${participantPayment?._id}`)
+    .then(res =>{
+        console.log(res.data)
+        if(res.data.modifiedCount > 0){
+            refetch();
+          toast.success(`${participantPayment.name}Participant Status Approved`)
+        }
+    })
+}
   return (
     <div>
       <div className="card  bg-base-100 shadow-xl">
         <div className="card-body">
           <h2 className="card-title">{campName}</h2>
+          <p>Name: {name}</p>
           <p>Email: {email}</p>
           <p>Phone: {phone}</p>
           <p>Health Info: {healthInfo}</p>
           <p>Gender: {gender}</p>
           <p>Emergency Contact: {emergencyContact}</p>
+          <p>TransactionId:{participantPayment?.transactionId}</p>
 
           <div className="card-actions justify-end">
             {participantPayment ? (
-              <button className="btn btn-primary w-full">{participantPayment.status}</button>
+              <button onClick={() => handleStatus(participantPayment?._id)} className="btn btn-primary w-full">{participantPayment.status}</button>
             ) : (
               <p>No payment information available</p>
             )}
